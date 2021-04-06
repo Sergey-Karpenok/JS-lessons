a04780ebf03bb2ce763d3ddd6d836fef1bd3257d
Sergey-Karpenok <Shamanito@rambler.ru>
2021-04-06 21:50:27 +0300

    Commplited

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 'use strict';
 
-const myString = function(str) {
+const isString = function(str) {
     if (typeof str !== 'string') {
         return alert(' Не является строкой ');
     } else {
-        console.log('Все хорошо');
+        str = str.trim();
+    }
+    if (str.length > 30) {
+        let newStr = str.slice(0, 29) + '...'
+        console.log(newStr);
+    } else {
+        console.log(str);
     }
 }
-
-myString(4444);
