053663736cb7bd60392cd2a7d5884103da452c81
Sergey-Karpenok <Shamanito@rambler.ru>
2021-04-06 12:46:19 +0300

    is it string

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-const
+const myString = function(str) {
+    if (typeof str !== 'string') {
+        return alert(' Не является строкой ');
+    } else {
+        console.log('Все хорошо');
+    }
+}
+
+myString(4444);
