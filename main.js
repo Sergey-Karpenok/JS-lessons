8cf053b94ab8aeb41f5ec8d676af2ffa69d32dc3
Sergey-Karpenok <Shamanito@rambler.ru>
2021-04-07 19:53:53 +0300

    fist task

--- main.js
+++ main.js
@@ -1,81 +1,17 @@
 'use strict';
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n)
-};
 
-let income = 'Инвестиции';
-let mission = 700000;
-let period = 6;
-let money;
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Детский сад, Алименты');
-let arr = addExpenses.toLowerCase().split(',');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let expenses = [];
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-const start = function() {
-    do {
-        money = prompt('Ваш месячный доход?');
-    } while (!isNumber(money))
-};
+let arr = [];
 
-start();
+for (let i = 0; i < 7; i++) {
+    arr[i] = getRandomInRange(12345, 9999999999999) + '3';
+}
 
-const getExpensesMonth = function() {
-    let sum = 0;
-    let num = 0;
-    for (let i = 0; i < 2; i++) {
-        expenses[i] = prompt('Введите обязательную статью расходов?', 'Комунальные расходы');
-        do {
-            num = prompt('Во сколько это обойдется?', '5000');
-        } while (!isNumber(num))
-        sum += +num;
-    }
-    return sum;
-};
-
-let expensesAmount = getExpensesMonth();
-let accumulatedMonth = getAccumulatedMonth();
-let budgetDay = Math.round(accumulatedMonth / 30);
-
-const getStatusIncome = function() {
-    switch (true) {
-        case (budgetDay >= 1200):
-            console.log('У вас высокий уровень дохода');
-            break;
-        case ((budgetDay >= 600) && (budgetDay < 1200)):
-            console.log('У вас средний уровень дохода');
-            break;
-        case ((budgetDay < 600) && (budgetDay >= 0)):
-            console.log('К сожалению у вас уровень дохода ниже среднего');
-            break;
-        case (budgetDay < 0):
-            console.log('Что то пошло не так');
-            break;
+for (let i = 0; i < 7; i++) {
+    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+        console.log(arr[i]);
     }
 }
-
-const showTypeOf = function(data) {
-    console.log(data, typeof(data));
-}
-
-function getAccumulatedMonth() {
-    return (money - expensesAmount);
-}
-const getTargetMonth = function() {
-
-    if ((mission / accumulatedMonth) > 0) {
-        console.log('Цель будет достигнута через ' + Math.round(mission / accumulatedMonth) + ' месяцев.');
-    } else {
-        console.log('Цель не будет достигнута');
-    }
-}
-
-// Вывод на экран
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-console.log('Расходы за месяц: ', expensesAmount);
-console.log('Возможные расходы за месяц: ', arr);
-getTargetMonth();
-console.log('Бюджет на день: ', budgetDay);
-getStatusIncome();
