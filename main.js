ce801c474a9186873fe3d64baccfaf323f274265
Sergey-Karpenok <Shamanito@rambler.ru>
2021-04-08 22:28:02 +0300

    использовал метод filter

--- main.js
+++ main.js
@@ -10,20 +10,28 @@ for (let i = 0; i < 7; i++) {
     arr[i] = getRandomInRange(12345, 9999999999999) + '3';
 }
 
-for (let i = 0; i < 7; i++) {
-    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
-        console.log(arr[i]);
-    }
+// for (let i = 0; i < 7; i++) {
+//     if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+//         console.log(arr[i]);
+//     }
+// }
+function startsWith(value) {
+    return value.startsWith('2') || value.startsWith('4');
 }
 
-top:
-    for (let i = 2; i <= 100; i++) {
-        let j = 2;
-        for (; j < i; j++) {
-            if ((i % j == 0) && (i != j)) {
-                continue top;
-            }
+let newArr = arr.filter(startsWith);
+console.log(arr);
+console.log('newArr: ', newArr);
 
-        }
-        console.log(`${i}: Простое число. Делители этого числа: 1 и `, j);
-    };
+
+// top:
+//     for (let i = 2; i <= 100; i++) {
+//         let j = 2;
+//         for (; j < i; j++) {
+//             if ((i % j == 0) && (i != j)) {
+//                 continue top;
+//             }
+
+//         }
+//         console.log(`${i}: Простое число. Делители этого числа: 1 и `, j);
+//     };
