913e9f02a6725a163277e8d50afc88f01d28f831
Sergey-Karpenok <Shamanito@rambler.ru>
2021-04-19 14:59:15 +0300

    В hard.js сделал 2-е обязательное

--- hard.js
+++ hard.js
@@ -1,1 +1,25 @@
 'use strict'
+
+class Fist {
+
+    hello() {
+        console.log(`Привет я метод родителя!`);
+    }
+}
+
+class Second extends Fist {
+    constructor() {
+        super();
+    }
+
+    hello() {
+        super.hello();
+        console.log(`А я наследуемый метод!`);
+    }
+}
+
+let fist = new Fist();
+let second = new Second();
+
+second.hello();
+console.log(second);
